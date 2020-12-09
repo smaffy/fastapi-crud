6070948370c9f484d479773092d6e97eaa57903e
smaffy <jekrudcz@gmail.com>
2020-12-09 16:43:17 +0200

    Additional Validation(id>0, title and desc must have lengths >= 3 and <= 50 for adding and updating a note)

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -22,6 +22,9 @@ def test_create_note_invalid_json(test_app):
     response = test_app.post("/notes/", data=json.dumps({"title": "something"}))
     assert response.status_code == 422
 
+    response = test_app.post("/notes/", data=json.dumps({"title": "1", "description": "2"}))
+    assert response.status_code == 422
+
 
 # GET Routes
 def test_read_note(test_app, monkeypatch):
@@ -41,9 +44,13 @@ def test_read_note_incorrect_id(test_app, monkeypatch):
         return None
 
     monkeypatch.setattr(crud, "get", mock_get)
+
     response = test_app.get("/notes/999")
     assert response.status_code == 404
     assert response.json()["detail"] == "Note not found"
+
+    response = test_app.get("/notes/0")
+    assert response.status_code == 422
 
 
 def test_read_all_notes(test_app, monkeypatch):
@@ -85,6 +92,9 @@ def test_update_note(test_app, monkeypatch):
         [1, {}, 422],
         [1, {"description": "bar"}, 422],
         [999, {"title": "foo", "description": "bar"}, 404],
+        [1, {"title": "1", "description": "bar"}, 422],
+        [1, {"title": "foo", "description": "1"}, 422],
+        [0, {"title": "foo", "description": "bar"}, 422],
     ],
 )
 def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
@@ -125,4 +135,7 @@ def test_remove_note_incorrect_id(test_app, monkeypatch):
     response = test_app.delete("/notes/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Note not found"
+
+    response = test_app.delete("/notes/0/")
+    assert response.status_code == 422
 
